76c011ef1fad502c4a2f3ad53383e52530095ae8
Cleiton Araujo <cleitonamc@gmail.com>
2025-02-25 00:59:01 -0300

    upgrade CategoryItens

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,8 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import '../models/category.dart';
 
 class CategoryItem extends StatelessWidget {
-
   final Category category;
 
   const CategoryItem({super.key, required this.category});
@@ -10,6 +11,14 @@ class CategoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [category.color.withOpacity(0.5), category.color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
       child: Text(category.title),
     );
   }
