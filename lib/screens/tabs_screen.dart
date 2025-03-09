718256052b2d8031dfebcb1a46d1790c998e16cc
Cleiton Araujo <cleitonamc@gmail.com>
2025-03-08 21:46:44 -0300

    mudança na TabBar (botton)

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -4,44 +4,56 @@ import 'package:flutter/material.dart';
 import 'categories_screen.dart';
 import 'favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0; //começa com a tela inicial
+
+  final List<Map<String, Object>> _screens = [
+    {'title': 'Categorias', 'screen': CategoriesScreen()},
+    {'title': 'Meus Favoritos', 'screen': FavoriteScreen()},
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     //Controlador de abas
-    return DefaultTabController(
-      length: 2, // indica q serão 2 abas
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Vamos Cozinhar?'),
-          //abaixo do titulo a TabBar contendo as 2 abas
-          bottom: TabBar(
-              labelColor: Colors.white, //cor da ativa
-              unselectedLabelColor: Colors.white70, // cor da não ativa
-              indicatorColor: Colors.white, // cor do tracinho
-              //indicatorSize: TabBarIndicatorSize.tab, // outro modelo de traço
-              indicatorWeight: 5, // altura do traço
-              // lista contendo o que sera exibido na tab 1 e 2
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.category),
-                  text: 'Categorias',
-                ),
-                Tab(
-                  icon: Icon(Icons.star),
-                  text: 'Favoritos',
-                )
-              ]),
-        ),
-        // no body temos a lista das telas que serão exibidas em cada aba
-        body: TabBarView(
-          children: [
-            CategoriesScreen(), 
-            FavoriteScreen(),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_screens[_selectedScreenIndex]['title'] as String),
       ),
+      // no body temos a lista das telas que serão exibidas em cada aba
+      body: _screens[_selectedScreenIndex]['screen'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Theme.of(context).colorScheme.primaryFixed,
+          selectedItemColor: Colors.pink,
+          unselectedItemColor: Colors.white,
+          // type: BottomNavigationBarType.shifting, faz um movimento quando selecionado
+          currentIndex: _selectedScreenIndex,
+          onTap:
+              _selectScreen, // ao toque ativa a função e passa o index do item clicado
+          items: [
+            BottomNavigationBarItem(
+              //backgroundColor: Theme.of(context).colorScheme.primaryFixed,
+              icon: Icon(Icons.category),
+              label: 'Categorias',
+            ),
+            BottomNavigationBarItem(
+              //backgroundColor: Theme.of(context).colorScheme.primaryFixed,
+              icon: Icon(Icons.star),
+              label: 'Favoritos',
+            )
+          ]),
     );
   }
 }
